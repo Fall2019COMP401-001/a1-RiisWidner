6d5e1620f803f98f168a6d7e8aebaae83adec17e
riiswidner <riiswidner@172.24.179.69>
2019-09-02 00:40:48 -0400

    trying to re upload

--- src/a1/A1Example.java
+++ src/a1/A1Example.java
@@ -12,7 +12,7 @@ public class A1Example {
 		// System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
 		// Set up scanner object to read space-separated values
-		// from console input
+		// from console inpu
 		
 		Scanner scan = new Scanner(System.in);
 
